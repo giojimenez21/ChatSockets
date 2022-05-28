7a5f55759e87badd8a59b7a1da3f3835919109e3
Alan Sandoval <alansandoval47@aragon.unam.mx>
2022-05-28 00:33:47 -0500

    Deploy to heroku.

--- src/database/prod.ts
+++ src/database/prod.ts
@@ -1,9 +1,8 @@
 const configProd= {
-    user: "root",
-    password:"",
-    db: "chat_socket",
-    host: "localhost"
+    user: "b59336addc194b",
+    password:"f70439ee",
+    db: "heroku_2902c74fb7ed4b4",
+    host: "us-cdbr-east-05.cleardb.net"
 }
-
 export default configProd;
 
