add2f58e1b61ab8888690fa4a20e0c1957073930
Alan Sandoval <alansandoval47@aragon.unam.mx>
2022-05-28 20:48:07 -0500

    Se modifica la conexion en el puerto del socket.

--- src/database/prod.ts
+++
@@ -1,8 +0,0 @@
-const configProd= {
-    user: "b59336addc194b",
-    password:"f70439ee",
-    db: "heroku_2902c74fb7ed4b4",
-    host: "us-cdbr-east-05.cleardb.net"
-}
-export default configProd;
-
